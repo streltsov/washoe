e57deb05cb29b05cdb840825a8d2c08621b1db24
streltsov <mail@streltsov.org>
2019-07-03 12:34:10 +0300

    Create card

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -22,9 +22,42 @@ const addWordToStorage = (word, meaning = '', example = '') =>
     },
   });
 
-const showCard = content => {
+function showCard(word) {
   const body = document.querySelector('body');
-  const modal = document.createElement('div');
-  modal.textContent = content;
-  body.appendChild(modal);
-};
+  const shadowRoot = document.createElement('div');
+  body.appendChild(shadowRoot);
+  const shadow = shadowRoot.attachShadow({mode: 'open'});
+
+  const style = document.createElement('style');
+  style.textContent = `.wsh-card{padding: 32px;border: 1px solid #ededf0;position: absolute; top: 10px; right: 10px;}`;
+  shadow.appendChild(style);
+
+  const card = document.createElement('div');
+  card.className = 'wsh-card';
+  shadow.appendChild(card);
+
+  const preWordSpan = document.createElement('p');
+  preWordSpan.className = 'wsh-pre-word';
+  preWordSpan.textContent = 'Do you remember what does this mean:';
+  card.appendChild(preWordSpan);
+
+  const wordSpan = document.createElement('span');
+  wordSpan.className = 'wsh-word';
+  wordSpan.textContent = word;
+  card.appendChild(wordSpan);
+
+  const buttons = document.createElement('div');
+
+  const buttonNo = document.createElement('button');
+  buttonNo.className = 'wsh-no-button';
+  buttonNo.textContent = 'No';
+  buttonNo.addEventListener('click', () => console.log('No Button'));
+  buttons.appendChild(buttonNo);
+
+  const buttonYes = document.createElement('button');
+  buttonYes.className = 'wsh-yes-button';
+  buttonYes.textContent = 'Yes';
+  buttonYes.addEventListener('click', () => console.log('Yes Button'));
+  buttons.appendChild(buttonYes);
+  card.appendChild(buttons);
+}
