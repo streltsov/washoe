3991badf11d801bebd5e92693d731accf5f79643
streltsov <mail@streltsov.org>
2019-07-06 00:04:25 +0300

    Style front side of the card

--- content_scripts/showCard.js
+++ content_scripts/showCard.js
@@ -20,21 +20,79 @@
     .wsh-card-scene,
     .wsh-card {
         width: 300px;
-        height: 120px;
+        height: 112px;
         }
     .wsh-card {
         position: relative;
         transition: transform 1s;
         transform-style: preserve-3d;
+
+        font-family: 'Fira Sans', sans-serif;
+        font-size: 16px;
+        font-weight: 400;
+        color: #0c0c0d;
+        border-radius: 2px;
+        border: 1px solid #e1e1e2;
     }
+    .wsh-card span {
+      display: block;
+      text-align: center;
+    }
+    .wsh-card button {
+      background-color: rgba(12, 12, 13, 0.1);
+      border: none;
+      border-radius: 2px;
+      height: 32px;
+      min-width: 132px;
+      padding: 0 8px;
+    }
+    .wsh-card button:hover {
+      background-color: rgba(12, 12, 13, 0.2);
+    }
+    .wsh-card button:active {
+      background-color: rgba(12, 12, 13, 0.3);
+    }
+    .wsh-card button:focus {
+      box-shadow: 0 0 0 1px #0a84ff inset, 0 0 0 1px #0a84ff, 0 0 0 4px rgba(10, 132, 255, 0.3)
+    }
+    .wsh-card button::-moz-focus-inner {
+      border: 0;
+    }
+
+    .wsh-word {
+      background-color: #ededf0;
+      padding: 8px 0;
+    }
+
+    .wsh-no-button {
+      margin: 0 4px;
+    }
+    .wsh-yes-button {
+      margin: 0 4px;
+    }
+    .wsh-card__front {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+}
+    .wsh-buttons {
+      display: flex;
+      justify-content: center;
+    }
+
+
+
+
+
     .wsh-card__front,
     .wsh-card__back {
         backface-visibility: hidden;
         position: absolute;
         height: 100%;
         width: 100%;
-        background-color: #fff;
+        background-color: #f9f9fa;
     }
+
     .wsh-card__back {
         transform: rotateY( -180deg );
     }
@@ -84,6 +142,7 @@ function showCard(word, meaning, example) {
   cardFront.appendChild(wordSpan);
 
   const buttons = document.createElement('div');
+  buttons.className = 'wsh-buttons';
   const buttonNo = document.createElement('button');
   buttonNo.className = 'wsh-no-button';
   buttonNo.textContent = 'No';
