2b220f84675168164863bda1bdc8f35cd10d3b43
streltsov <mail@streltsov.org>
2019-07-06 20:45:29 +0300

    Remove wsh prefix

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -4,7 +4,7 @@ browser.runtime.onMessage.addListener(request => {
   if (request.data) {
     document
       .querySelector('#wsh-card-container')
-      .shadowRoot.querySelector(`.wsh-${request.data.word.replace(/\s/g, '_')}`)
+      .shadowRoot.querySelector(`.${request.data.word.replace(/\s/g, '_')}`)
       ? null
       : showCard(request.data.word, request.data.meaning, request.data.example);
   }
