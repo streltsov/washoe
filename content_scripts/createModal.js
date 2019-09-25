b3f26b61fd2ad67a3eb0f52c71d71abe9fb86145
streltsov <mail@streltsov.org>
2019-09-25 23:43:50 +0300

    Renew modal window

--- content_scripts/createModal.js
+++ content_scripts/createModal.js
@@ -1,33 +1,27 @@
 'use strict';
 
 const createModal = word => {
-  const modal = createElement('div', 'modal');
+  const modal = createElement('form', 'modal');
 
-  const wordInput = createElement('input', 'word-input');
-  wordInput.value = word.toLowerCase();
+  [
+    `<input type="submit" value="Done">`,
+    `<textarea placeholder='Example (optional)'></textarea>`,
+    `<textarea required="true" placeholder='Meaning' autofocus="true"></textarea>`,
+    `<textarea required="true" rows="1">${word.toLowerCase()}</textarea>`,
+  ].forEach(el => modal.insertAdjacentHTML('afterbegin', el));
 
-  const meaning = createElement('input', 'meaning');
-  meaning.placeholder = 'Meaning';
-
-  const example = createElement('input', 'example');
-  example.placeholder = 'Example (optional)';
-
-  const addButton = createElement('button', 'add-button', 'Add');
-  addButton.addEventListener('click', () => {
-    addWordToStorage(
-      wordInput.value.trim().toLowerCase(),
-      meaning.value.trim(),
-      example.value.trim(),
-    );
+  modal.addEventListener('submit', event => {
+    event.preventDefault();
+    addWordToStorage(...Array.from(modal.elements, el => el.value.trim()));
     removeShadowDom();
   });
-  [wordInput, meaning, example, addButton].forEach(el => modal.appendChild(el));
+
   return modal;
 };
 
-const modalStyles = `.modal{display:flex;justify-content:space-around;align-items:center;flex-wrap:wrap;width:100%;background-color:#0c0c0d;position:fixed;top:0;left:0;height:min-content;z-index:2147483647}input{padding:4px;background-color:#2a2a2e;color:#fff;border:none;margin:8px;}button{background-color:#2a2a2e;color:#fff;border:none;height:32px}`;
+const modalStyles = `.modal{position:fixed;top:8px;right:8px;display:flex;flex-direction:column;justify-content:space-around;align-items:center;width:min-content;z-index:2147483647;padding:12px 12px 0;background-color:#0c0c0d}textarea:nth-child(1){height: 35px;resize:none}textarea:nth-child(1):invalid,textarea:nth-child(2):invalid{border-color:#d70022;box-shadow:0 0 0 1px #d70022,0 0 0 4px rgba(251,0,34,.3)}input,textarea{color:#fff;border:2px solid #2a2a2e;background-color:#2a2a2e;margin-bottom:12px;width:280px;border-radius:2px;box-sizing:border-box}textarea{padding:8px}input{height:32px}input::-moz-focus-inner{border:0}input:focus,textarea:focus{border-color:#0a84ff!important;box-shadow:0 0 0 1px #0a84ff,0 0 0 4px rgba(10,132,255,.3)!important}`;
 
-const addWordToStorage = (word, meaning = '', example = '') =>
+const addWordToStorage = (word, meaning, example) =>
   browser.storage.sync.set({
     [word]: {
       meaning,
