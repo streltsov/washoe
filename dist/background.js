b16ff3c5b24edf6e83998080f9f5752c28d7a5b3
Herman Streltsov <mail@streltsov.org>
2019-11-04 21:26:36 +0300

    Add dist

---
+++ dist/background.js
@@ -0,0 +1,44 @@
+const INTERVALS = [
+  120000,
+  600000,
+  3600000,
+  18000000,
+  86400000,
+  432000000,
+  2160000000,
+  10368000000,
+];
+const isItTimeToReview = word => Date.now() - word.time > INTERVALS[word.stage];
+const checkWords = storage =>
+  Object.keys(storage).forEach(
+    word =>
+      isItTimeToReview(storage[word]) &&
+      sendMessageToActiveTab(
+        word,
+        storage[word].meaning,
+        storage[word].example,
+      ),
+  );
+const sendMessageToActiveTab = (word, meaning = '', example = '') =>
+  browser.tabs
+    .query({
+      currentWindow: true,
+      active: true,
+    })
+    .then(tabs =>
+      browser.tabs.sendMessage(tabs[0].id, {
+        wordData: {word, meaning, example},
+      }),
+    );
+
+browser.runtime.onMessage.addListener(() =>
+  browser.tabs.query({}).then(tabs =>
+    tabs.forEach(tab => {
+      browser.tabs.executeScript(tab.id, {
+        code: `document.querySelector('.wsh-shadow-root-card').remove()`,
+      });
+    }),
+  ),
+);
+
+setInterval(() => browser.storage.sync.get().then(checkWords), 60000);
