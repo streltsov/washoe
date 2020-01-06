2807187ed447327d944611413ef8a055e1db551f
Herman Streltsov <mail@streltsov.org>
2020-01-06 14:50:42 +0300

    Add createElement to dom utils

--- src/content_scripts/dom-utils.js
+++ src/content_scripts/dom-utils.js
@@ -1,3 +1,15 @@
+export const createElement = (el = 'div', text = '') => {
+  const element = document.createElement(el.split('.')[0]);
+  el.split('.')[1]
+    ? (element.className = el
+        .split('.')
+        .slice(1)
+        .join(' '))
+    : null;
+  element.textContent = text;
+  return element;
+};
+
 export const styleElement = (element, styles) =>
   Object.keys(styles).forEach(prop => (element.style[prop] = styles[prop]));
 
