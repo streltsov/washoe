4e358156b0899e1ff8fb4bdb7209dc5528e90a79
Herman Streltsov <mail@streltsov.org>
2020-01-12 22:38:02 +0300

    Start styling card

--- src/content_scripts/components/Card.js
+++ src/content_scripts/components/Card.js
@@ -2,21 +2,44 @@ import {createElement, styleElement} from '../dom-utils';
 import {changeWordStage} from '../utils';
 
 const wrapperStyles = {
-  position: 'absolute',
-  top: 0,
+  position: 'fixed',
+  bottom: 0,
   left: 0,
-  backgroundColor: '#000',
-  color: '#fff',
+  backgroundColor: 'rgb(0, 0, 0)',
+  color: 'rgb(255, 255, 255)',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
 const Card = ({word, meaning, examples, stage}) => {
-  console.log('Stage: ', stage);
   const wrapper = createElement('div.washoe-card');
   styleElement(wrapper, wrapperStyles);
 
   if (stage % 2) {
     const visible = createElement('div', word);
+    visible.addEventListener('click', () => {
+      const removeInterval = setInterval(expandHiddenBlock, 2);
+
+      let height = 0;
+      function expandHiddenBlock() {
+        height == 240 && removeInterval();
+        hidden.style.height = height + 'px';
+        height++;
+      }
+    });
+    styleElement(visible, {
+      display: 'flex',
+      alignItems: 'center',
+      height: '48px',
+      fontSize: '18px',
+      fontWeight: 500,
+    });
+
     const hidden = createElement();
+    styleElement(hidden, {height: '0', overflow: 'hidden'});
     const meaningSpan = createElement('span', meaning);
     const buttons = createElement();
     const againButton = createElement('button', 'Again');
