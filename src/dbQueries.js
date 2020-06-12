09c8c1ae7c406874bbf317000c17bc92f6c58b3d
Herman Streltsov <mail@streltsov.dev>
2020-06-12 19:36:55 +0300

    Create resetStage function

--- src/dbQueries.js
+++ src/dbQueries.js
@@ -4,6 +4,9 @@ const { Pool, Client } = require('pg')
 exports.updateStage = word => 
   pool.query('UPDATE words SET stage = $1, timestamp = $2 WHERE email = $3 AND word_id = $4', [word.stage + 1, new Date( Date.now() + word.notifyIn), word.email, word.word_id]).then(res => res.rows[0]);
 
+exports.resetStage = word => 
+  pool.query('UPDATE words SET stage = 0, timestamp = $1 WHERE email = $2 AND word_id = $3', [new Date( Date.now() + 12e4), word.email, word.word_id]).then(res => res.rows[0]);
+
 exports.getScheduledWord = email =>
   pool.query('SELECT * FROM words WHERE email = $1 AND timestamp < current_timestamp ORDER BY timestamp LIMIT 1', [email]).then(res => res.rows);
 
