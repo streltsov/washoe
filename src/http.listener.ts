aaaf78e85743e6ea9a71265d427e037d84235808
Herman Streltsov <mail@streltsov.dev>
2020-08-02 20:52:34 +0300

    Apply CORS middleware

--- src/http.listener.ts
+++ src/http.listener.ts
@@ -1,11 +1,17 @@
-import { httpListener } from '@marblejs/core';
-import { logger$ } from '@marblejs/middleware-logger';
 import { bodyParser$ } from '@marblejs/middleware-body';
+import { logger$ } from '@marblejs/middleware-logger';
+import { cors$ } from '@marblejs/middleware-cors';
+import { httpListener } from '@marblejs/core';
 import { api$ } from './api';
 
 const middlewares = [
   logger$(),
   bodyParser$(),
+  cors$({
+    origin: '*',
+    allowHeaders: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  })
 ];
 
 const effects = [
