db6ab2ed47d245d30fe7251abd3e031a7b94a4b8
Herman Streltsov <mail@streltsov.dev>
2021-04-10 22:45:49 +0300

    Remove api.effect.ts

--- src/api.effect.ts
+++
@@ -1,9 +0,0 @@
-import { mapTo } from "rxjs/operators";
-import { r } from "@marblejs/core";
-
-export const api$ = r.pipe(
-  r.matchPath("/"),
-  r.matchType("GET"),
-  r.useEffect(req$ => req$.pipe(
-    mapTo({ body: "Hello, world!" }),
-  )));
