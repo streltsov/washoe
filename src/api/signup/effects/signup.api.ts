de0a1f068ed4205897cf600f8d823b76a10a5a5f
Herman Streltsov <mail@streltsov.dev>
2021-07-13 19:39:15 +0300

    Move effect to api

--- src/api/signup/effects/signup.api.ts
+++ src/api/signup/effects/signup.api.ts
@@ -1,6 +1,26 @@
-import { r } from '@marblejs/core';
-import { signupEffect$ } from './signup.effect';
+import { HttpError, HttpStatus, HttpEffect, use, r } from '@marblejs/core';
+import { requestValidator$, t } from '@marblejs/middleware-io';
+import { UserModel as User } from '../../users/model/users.dao';
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
+const validator$ = requestValidator$({
+  body: t.type({
+    email: t.string,
+    password: t.string
+  })
+});
+
+const signupEffect$: HttpEffect = req$ =>
+  req$.pipe(
+    use(validator$),
+    map(({ body }) => body),
+    switchMap(user => User.create(user)),
+    map(response => ({ body: response })),
+    catchError(() => throwError(
+      new HttpError('Bad Request', HttpStatus.BAD_REQUEST)
+    )),
+  )
 
 export const signup$ = r.pipe(
   r.matchPath('/signup'),
